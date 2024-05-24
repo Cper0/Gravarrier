6375bcf82d2061619f7d3c7213b5c216fe856204
root <root@BlueSky>
2024-05-24 19:57:25 +0900

    fixed Atomic class

--- src/Atomic.cpp
+++ src/Atomic.cpp
@@ -1,4 +1,6 @@
 #include"Atomic.hpp"
+#include<cmath>
+#include<iostream>
 
 namespace sha
 {
@@ -9,40 +11,46 @@ namespace sha
 		vari = v;
 	}
 
-	void Atomic::force(value_t f, const Atomic& t)
+	void Atomic::force(value_t f, const Atomic& b)
 	{
-		auto s = pos;
-		auto t = pos + f;
+		const auto n = pos - b.p();
+
+		value_t s = n;
+		value_t t = n + f;
 		for(int i = 0; i < 10; i++)
 		{
 			const auto m = (s + t) / 2;
-			const auto L = length(pos, m);
+			const auto L = b.length(n, m);
 
 			if(L > std::abs(f)) t = m;
 			else s = m;
 		}
 
-		pos = (s + t) / 2;
+		const auto q = (s + t) / 2;
+		const auto d = derivate(q);
+
+		pos = b.p() + q - d;
 	}
 
-	float Atomic::length(float a, float b)
+	Atomic::value_t Atomic::length(value_t a, value_t b) const
 	{
 		auto integ = [&](value_t x) -> value_t
 		{
 			const float df = derivate(x);
 			const float b = 1.0 + df * df;
 			const float y = 2.0f / 3.0f * b * std::sqrt(b);
+			return y;
 		};
 
 		return std::abs(integ(b)) + std::abs(integ(a));
 	}
 
-	value_t Atomic::dist(value_t x) const
+	Atomic::value_t Atomic::dist(value_t x) const
 	{
-		return m * std::exp(-x * x / vari);
+		return mass * std::exp(-x * x / vari);
 	}
 
-	value_t Atomic::derivate(value_t x) const
+	Atomic::value_t Atomic::derivate(value_t x) const
 	{
 		constexpr float eps = 0.001f;
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include<Siv3D.hpp>
 #include"Atomic.hpp"
 
+constexpr float r = 3;
+
 void Main()
 {
 	Window::Resize(640, 400);
@@ -10,15 +12,15 @@ void Main()
 	constexpr float cam = -100;
 	const float f = json[U"f"].get<float>();
 
-	Atomic a(320, 10, 1);
-	Atomic b(100, 10, 1);
+	sha::Atomic a(320, 10, 1);
+	sha::Atomic b(100, 10, 1);
 
 	while(System::Update())
 	{
 		Line{0, 200, 640, 200}.draw(ColorF{1.0f});
 
-		Circle{a.p() - cam, 200, r}.draw(ColorF{1, 0, 0});
-		Circle{b.p() - cam, 200, r}.draw(ColorF{0, 0, 1});
+		Circle{a.p(), 200, r}.draw(ColorF{1, 0, 0});
+		Circle{b.p(), 200, r}.draw(ColorF{0, 0, 1});
 
 		if(KeyLeft.pressed())
 		{
